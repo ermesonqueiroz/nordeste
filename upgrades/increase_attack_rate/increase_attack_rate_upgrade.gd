11b460a36821e846bd01ddb4a14623a806aa5a54
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2026-09-14 20:26:38 -0300

    feat: add increase attack speed icon

--- upgrades/increase_attack_rate/increase_attack_rate_upgrade.gd
+++ upgrades/increase_attack_rate/increase_attack_rate_upgrade.gd
@@ -5,6 +5,7 @@ func _init() -> void:
 	id = "increase_attack_speed"
 	name = "Tiro rápido"
 	description = "Aumenta o attack rate do revolver em 20%."
+	icon = preload("res://upgrades/increase_attack_rate/icon.png")
 	max_uses = 4
 
 func apply_upgrade(_character: BaseCharacter) -> void:
